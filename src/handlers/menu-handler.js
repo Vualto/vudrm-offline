d0a7732f50898c10f064c7ea73f6400b18bc420b
Mark Niland <mark.niland@vualto.com>
2019-06-17 11:05:42 +0100

    fix file not showing on macOS

---
+++ src/handlers/menu-handler.js
@@ -0,0 +1,40 @@
+const { app, Menu } = require('electron');
+const WindowHandler = require('./window-handler');
+const path = require('path');
+
+let windowHandler = new WindowHandler();
+let VIEWS_DIR = path.join(__dirname + '/../views/');
+
+class AppMenuHandler {
+    constructor() {
+        let menuTemplate = [{
+            label: 'File',
+            submenu: [
+                {
+                    label: 'load stream',
+                    accelerator: process.platform === 'darwin' ? 'Command+L' : 'Ctrl+L',
+                    click() {
+                        let addWindow = windowHandler.createWindow("Add stream", 500, 200)
+                        windowHandler.setWindowEvents(addWindow);
+                        windowHandler.loadWindow(addWindow, path.join(VIEWS_DIR + 'add_stream.html'));
+                    }
+                },
+                {
+                    label: 'quit',
+                    accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                    click() {
+                        app.quit();
+                    }
+                }
+            ]
+        }];
+
+        if (process.platform === 'darwin') {
+            menuTemplate.unshift({});
+        }
+        Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+    }
+
+}
+
+module.exports = AppMenuHandler;
